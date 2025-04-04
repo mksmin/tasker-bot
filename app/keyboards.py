0030d73f0695ecf9b05c356e2b33464785b12237
mks_min <mininname@vk.com>
2025-04-04 13:54:58 +0300

    Update: update keyboard

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,9 +1,9 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 finishing_task = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="Удалить аффирмацию", callback_data="finish_task")]
     ])
 
 list_of_tasks = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="Все аффирмации", callback_data="my_tasks")],
+    [InlineKeyboardButton(text="Все аффирмации", web_app=WebAppInfo(url="https://api.mks-min.ru/affirm"))],
 ])
