e0da703475f07a28d40a741f193d7466a40ffcd2
mks_min <mininname@vk.com>
2025-07-05 16:40:48 +0300

    feat(managers): initialize TaskManager in CRUDManager

--- database/crud/managers/user.py
+++ database/crud/managers/user.py
@@ -1,13 +1,19 @@
+# import from libs
+from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
+
+# import from modules
 from .base import BaseCRUDManager
 from database.models import User
 from database.schemas import UserCreateSchema
-from database import db_helper
 
 
 class UserManager(BaseCRUDManager[User]):
 
-    def __init__(self):
-        super().__init__(model=User, session_maker=db_helper.session_factory)
+    def __init__(self, session_maker: async_sessionmaker[AsyncSession]):
+        super().__init__(
+            model=User,
+            session_maker=session_maker,
+        )
 
     async def create_user(self, user_data: dict):
         instance = UserCreateSchema(**user_data)
