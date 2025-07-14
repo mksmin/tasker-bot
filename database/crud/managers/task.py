bc99c0c4fda9d41ba7252f86fa5dc4b03e8d2694
mks_min <mininname@vk.com>
2025-07-15 00:12:32 +0300

    feat(managers): use pydantic schema in create_task

--- database/crud/managers/task.py
+++ database/crud/managers/task.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 from .base import BaseCRUDManager
 from .user import UserManager
 from database.models import Task
-from database.schemas import UserCreateSchema, TaskCreateSchema, UserReadSchema
+from database.schemas import TaskCreateSchema, TaskReadSchema, UserReadSchema
 
 
 class TaskManager(BaseCRUDManager[Task]):
@@ -19,14 +19,12 @@ class TaskManager(BaseCRUDManager[Task]):
     def set_user_manager(self, manager: "UserManager"):
         self.user_manager = manager
 
-    async def create_task(self, user_tg: int, task_text: str):
-        user = await self.user_manager.get(user_tg=user_tg)
-        user_schema = UserReadSchema.model_validate(user)
-        print(f'user_schema: {user_schema}')
-        # return await user_schema
+    async def create_task(self, user_tg: int, task_text: str) -> TaskReadSchema:
+        user = await self.user_manager.get_user(user_tg=user_tg)
+
         task_schema = TaskCreateSchema(
             text_task=task_text,
-            user_id=user_schema.id
+            user_id=user.id,
         )
-
-        return await self.create(data=task_schema)
+        created_model = await self.create(data=task_schema)
+        return TaskReadSchema.model_validate(created_model)
