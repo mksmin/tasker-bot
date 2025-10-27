ba3d291d80567046b8d1dc44688a72aa99628c2d
mks_min <mininname@vk.com>
2025-10-27 20:42:39 +0300

    feat: create base manager

---
+++ bot-app/crud/managers/__init__.py
@@ -0,0 +1,1 @@
+from crud.managers.base import BaseCRUDManager as BaseCRUDManager
---
+++ bot-app/crud/managers/base.py
@@ -0,0 +1,38 @@
+import logging
+from typing import Generic, TypeVar
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database import Base
+
+log = logging.getLogger(__name__)
+ModelType = TypeVar("ModelType", bound=Base)
+
+
+class BaseCRUDManager(Generic[ModelType]):
+    def __init__(
+        self,
+        session: AsyncSession,
+        model: type[ModelType],
+    ) -> None:
+        self.session = session
+        self.model = model
+
+    async def get(
+        self,
+        obj_id: int,
+    ) -> ModelType | None:
+        return await self.session.get(self.model, obj_id)
+
+    async def add(
+        self,
+        obj: ModelType,
+    ) -> ModelType:
+        self.session.add(obj)
+        return obj
+
+    async def remove(
+        self,
+        obj_id: int,
+    ) -> None:
+        pass
